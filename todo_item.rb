4d776abe5f2c07747c2bf8f9c99740f7821988ed
kscotteng <kscotteng@gmail.com>
2016-06-12 20:49:07 -0400

    create methods to mark ToDoItem complete or incomplete

--- todo_item.rb
+++ todo_item.rb
@@ -8,4 +8,15 @@ class ToDoItem
     @complete = false
   end
 
+  # create a method to mark the ToDoItem complete
+  # when a method changes a receiver, it can be considered a bang method
+  def mark_complete!
+    @complete = true
+  end
+
+  # create a method to mark the ToDoItem incomplete
+  def mark_incomplete!
+    @complete = false
+  end
+
 end
