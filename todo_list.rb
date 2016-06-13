6048e51b328eb2c9ba77052faffb5bd7d9c3836e
kscotteng <kscotteng@gmail.com>
2016-06-12 21:35:25 -0400

    create add_item method to ToDoList that appends TodoItems to ToDoList

--- todo_list.rb
+++ todo_list.rb
@@ -10,18 +10,25 @@ class ToDoList
     @todo_items = []
   end
 
+  #add method that appends TodoItems to ToDoList
+  def add_item(name)
+    todo_items << (ToDoItem.new(name))
+  end
+
 end
 
 # instantiating ToDoList class by:
 #     adding "Groceries" to todo_list, and
 #     adding "Milk" to todo_item
-# todo_list = ToDoList.new("Groceries")
-todo_item = ToDoItem.new("Milk")
+todo_list = ToDoList.new("Groceries")
+todo_list.add_item("Milk")
+# todo_item = ToDoItem.new("Milk")
 
-# puts todo_list.inspect
-# puts todo_list.inspect
+puts todo_list.inspect
+# puts todo_item.inspect
 
-puts todo_item
-
-todo_item.mark_complete!
-puts todo_item
+# puts todo_item
+#
+# todo_item.mark_complete!
+# puts todo_item
+# todo_item.mark_incomplete
